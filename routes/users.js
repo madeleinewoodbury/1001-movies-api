3cd0e7182c84f874e6fced461eb3612368e78930
madeleinewoodbury <randine25@gmail.com>
2020-08-01 22:41:08 -0400

    User model and auth middleware

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,54 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const User = require('../models/User');
 
-// @desc    Test route
-// @route   GET /api/v1/users/
+// @desc    Create a user
+// @route   POST /api/v1/users
 // @access  Public
-router.get('/', async (req, res) => {
-  res.json({
-    success: true,
-    msg: 'Users route',
-  });
-});
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please enter a valid email address').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 characters or more'
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      // Check if user exists
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exist' }] });
+      }
+
+      // Create new user
+      user = new User({ name, email, password });
+      await user.save();
+
+      // Create token
+      const token = user.getSignedJwtToken();
+
+      res.status(201).json({
+        success: true,
+        token,
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
